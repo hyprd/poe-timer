fe052ef0d4301796e83987f229d7da3899c25e23
hyprd <luuzeish@gmail.com>
2024-01-03 00:57:57 +1300

    Returns last line of the client file

--- watcher.py
+++ watcher.py
@@ -1,12 +1,23 @@
 from tkinter.filedialog import askdirectory
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers import Observer
-import time
+import time, os
 
 class FileModifiedHandler(PatternMatchingEventHandler):
     def on_modified(self, event):
-        print("File was modified.")
+        new_line = seek_last_line(self.patterns[0])
+        # print(new_line)
         return super().on_modified(event)
+
+def seek_last_line(client):
+    with open(client, "rb") as file:
+        try:
+            file.seek(-2, os.SEEK_END)
+            while file.read(1) != b'\n':
+                file.seek(-2, os.SEEK_CUR)
+        except OSError:
+            file.seek(0)
+        return file.readline().decode()
 
 def main():
     directory = askdirectory(title = 'Path to client.txt')
@@ -21,7 +32,6 @@ def main():
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
-    
     
 if __name__ == '__main__':
     main()
