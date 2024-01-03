a60fa2e5dea21e23cac74596204b17f2aa96e71f
hyprd <luuzeish@gmail.com>
2024-01-03 18:16:20 +1300

    Successfully calculates time delta to console

--- watcher.py
+++ watcher.py
@@ -1,13 +1,25 @@
 from tkinter.filedialog import askdirectory
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers.polling import PollingObserver
+
 import time, os
+
+timer_started = False
+start = 0
+elapsed_time = 0
 
 class FileModifiedHandler(PatternMatchingEventHandler):
     def on_modified(self, event):
+        global elapsed_time, start, timer_started
         new_line = seek_last_line(self.patterns[0])
         if "You have entered" in new_line:
-            print(new_line)
+            if not timer_started:
+                start = time.perf_counter()
+                timer_started = True
+            elif timer_started:
+                elapsed_time = round(time.perf_counter() - start, 2)
+                print(elapsed_time)
+                timer_started = False
         return super().on_modified(event)
 
 def seek_last_line(client):
