b5240c292a0668b7aa1346c85b2186aecc109f82
hyprd <luuzeish@gmail.com>
2024-01-05 15:49:37 +1300

    Basic logging functionality added

--- watcher.py
+++ watcher.py
@@ -1,10 +1,12 @@
 from tkinter.filedialog import askdirectory
 from watchdog.events import PatternMatchingEventHandler
 from watchdog.observers.polling import PollingObserver
+from datetime import datetime
+from collections import deque
 
+import logging
 import tkinter as tk
 import time
-from collections import deque
 
 window = tk.Tk()
 window.attributes('-topmost', True)
@@ -17,9 +19,8 @@ time_label.pack()
     
 client = ''
 current_instance = ''
+timestamp = ''
 timer_started = False
-elapsed_time = 0
-start_time = 0
 stopwatch_time = 0
 
 def timer():
@@ -30,7 +31,7 @@ def timer():
     
 class FileModifiedHandler(PatternMatchingEventHandler):
     def on_modified(self, event):
-        global client, current_instance, elapsed_time, start_time, timer_started, stopwatch_time
+        global client, current_instance, timer_started, stopwatch_time, timestamp
         with open(client, encoding='utf-8') as file:
             client_contents = deque(file, 2)
         # The watchdog poller sometimes skips over the instance transition string. Deque for the 
@@ -38,16 +39,17 @@ class FileModifiedHandler(PatternMatchingEventHandler):
         for i in range(2):    
             if "You have entered" in client_contents[i] and client_contents[i] != current_instance:
                 current_instance = client_contents[i]
+                current_instance_formatted = client_contents[i].partition('entered ')[2].replace('.', '').strip()
                 stopwatch_time = 0
                 if not timer_started:
                     timer_started = True
-                    start_time = time.perf_counter()
+                    timestamp = datetime.now()
                     timer()
                 elif timer_started:
                     timer_started = False
-                    elapsed_time = time.perf_counter() - start_time
-                
-                instance_label['text'] = client_contents[i].partition('entered ')[2].replace('.', '')
+                    elapsed = str(datetime.now() - timestamp)
+                    logging.info("Entered %s for %s", current_instance_formatted, elapsed)
+                instance_label['text'] = current_instance_formatted
             else:
                 continue
                
@@ -60,7 +62,7 @@ def main():
     observer = PollingObserver()
     observer.schedule(event_handler, directory)
     observer.start()
-    
+    logging.basicConfig(filename = 'times.log', filemode = 'a', datefmt = '%H:%M:%S', format='[%(asctime)s] %(message)s', level = logging.INFO)
     window.mainloop()
     try:
         while True:
